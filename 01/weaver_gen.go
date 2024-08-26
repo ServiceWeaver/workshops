bcdb3aa9582f54a7416e28e68b2f183f5592d58c
Robert Grandl <rgrandl@google.com>
2024-08-26 14:32:15 -0700

    Update weaver version to 0.24.4

--- 01/weaver_gen.go
+++ 01/weaver_gen.go
@@ -60,7 +60,7 @@ var _ weaver.Main = (*main_client_stub)(nil)
 // you run "go build" or "go run".
 var _ codegen.LatestVersion = codegen.Version[[0][24]struct{}](`
 
-ERROR: You generated this file with 'weaver generate' v0.23.2 (codegen
+ERROR: You generated this file with 'weaver generate' v0.24.4 (codegen
 version v0.24.0). The generated code is incompatible with the version of the
 github.com/ServiceWeaver/weaver module that you're using. The weaver module
 version can be found in your go.mod file or by running the following command.
